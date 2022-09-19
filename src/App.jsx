22b4a3e721808d723b560e1b30fe67642f6294f8
Tommy Oliveir <laridatommy@gmail.com>
2022-09-19 16:22:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import InputForm from "./components/InputForm";
+
+function App() {
+  return (
+    <div className="App">
+      form
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
